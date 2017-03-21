a51fca4bdda91453c3ad16d7bf022ee51af163e4
Galin McMahon and Mohamed Warsame <galinmcmahon@gmail.com>
2017-03-21 15:03:20 -0700

    add borders around flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#JavaScript").click(function() {
+    $("#JavaScript-front").toggle();
+    $("#JavaScript-back").toggle();
+
+  });
+
+  $("#Operators").click(function() {
+    $("#Operators-front").toggle();
+    $("#Operators-back").toggle();
+  });
+
+$("#Variables").click(function() {
+  $("#Variables-front").toggle();
+  $("#Variables-back").toggle();
+});
+
+$("#Functions").click(function() {
+  $("#Functions-front").toggle();
+  $("#Functions-back").toggle();
+});
+});
