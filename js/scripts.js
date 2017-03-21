62894402aeb79b4c8eead2bc41f320d0b5bd1f38
Galin McMahon and Mohamed Warsame <mwarsame9@gmail.com>
2017-03-21 15:40:43 -0700

    Add all cards.  Add borders.  Add jumbotron.

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,68 @@ $("#Functions").click(function() {
   $("#Functions-front").toggle();
   $("#Functions-back").toggle();
 });
+
+$("#Methods").click(function() {
+  $("#Methods-front").toggle();
+  $("#Methods-back").toggle();
+
+});
+
+$("#Arguments").click(function() {
+  $("#Arguments-front").toggle();
+  $("#Arguments-back").toggle();
+});
+
+$("#Parameters").click(function() {
+$("#Parameters-front").toggle();
+$("#Parameters-back").toggle();
+});
+
+$("#Return").click(function() {
+$("#Return-front").toggle();
+$("#Return-back").toggle();
+});
+
+$("#Strings").click(function() {
+  $("#Strings-front").toggle();
+  $("#Strings-back").toggle();
+
+});
+
+$("#Booleans").click(function() {
+  $("#Booleans-front").toggle();
+  $("#Booleans-back").toggle();
+});
+
+$("#Undefined").click(function() {
+$("#Undefined-front").toggle();
+$("#Undefined-back").toggle();
+});
+
+$("#Escape").click(function() {
+$("#Escape-front").toggle();
+$("#Escape-back").toggle();
+});
+
+$("#Alert").click(function() {
+$("#Alert-front").toggle();
+$("#Alert-back").toggle();
+
+});
+
+$("#Comments").click(function() {
+$("#Comments-front").toggle();
+$("#Comments-back").toggle();
+});
+
+$("#jQuery").click(function() {
+$("#jQuery-front").toggle();
+$("#jQuery-back").toggle();
+});
+
+$("#Attributes").click(function() {
+$("#Attributes-front").toggle();
+$("#Attributes-back").toggle();
+});
+
 });
